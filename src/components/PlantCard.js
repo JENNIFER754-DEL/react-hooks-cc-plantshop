7ff37a1fa3a7006ea1770dbd5274a0c912330ddc
JENNIFER754-DEL <jennifernyaguthii23@gmail.com>
2025-04-25 23:06:40 +0300

    third attempt

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,15 +1,33 @@
 import React from "react";
 
-function PlantCard() {
+function PlantCard({ plant, onUpdatePlant }) {
+  function handleSoldOutClick() {
+    const updatedPlant = { ...plant, soldOut: !plant.soldOut };
+
+    fetch("http://localhost:6001/plants/" + plant.id, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ soldOut: updatedPlant.soldOut })
+    })
+      .then((r) => r.json())
+      .then((data) => {
+        onUpdatePlant(data);
+      });
+  }
+
   return (
     <li className="card" data-testid="plant-item">
-      <img src={"https://via.placeholder.com/400"} alt={"plant name"} />
-      <h4>{"plant name"}</h4>
-      <p>Price: {"plant price"}</p>
-      {true ? (
-        <button className="primary">In Stock</button>
+      <img src={plant.image} alt={plant.name} />
+      <h4>{plant.name}</h4>
+      <p>Price: ${plant.price.toFixed(2)}</p>
+      {plant.soldOut ? (
+        <button onClick={handleSoldOutClick}>Out of Stock</button>
       ) : (
-        <button>Out of Stock</button>
+        <button className="primary" onClick={handleSoldOutClick}>
+          In Stock
+        </button>
       )}
     </li>
   );
